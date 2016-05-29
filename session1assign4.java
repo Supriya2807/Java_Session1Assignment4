a36c4b0f52d22b37b73ae2fe34736505cf01450a
Supriya2807 <lahadesupriya@gmail.com>
2016-05-29 08:02:26 +0530

    session1assign4
    session1assign4

---
+++ session1assign4.java
@@ -0,0 +1,27 @@
+package acadgild.assignments;
+
+public class session1assign4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=5,y=10;
+		int value;
+		
+		value= x+y*2;
+		System.out.println("The value of x+y*2 is : "+value);
+		
+		value=x-y+2;
+		System.out.println("The value of x-y+2 is : "+value);
+		
+		value=(x+y)*2;
+		System.out.println("The value of (x+y)*2 is : "+value);
+		
+		value= y%x;
+		System.out.println("The value of y%x is : "+value);
+
+
+
+	}
+
+}
